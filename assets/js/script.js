d9954786bdd9af6dce0a4ae3a905096e81a910e1
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-09-24 23:45:30 +0900

    20200924 portfolio website ver.1

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function headerToggle() {
+
+    let header = document.getElementById("headerNav");
+    if (header.className === "header_Nav") {
+      header.className += " responsive";
+    } else {
+      header.className = "header_Nav";
+    }
+  }
