39506fce1916ad9874b839046200cb6282f0d0f6
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-09-28 23:49:28 +0900

    200928 update
    1. 프로젝트 보더라인 수정
    2. 상단 네비바 반응형일때 클릭하면 사라지지 않던것 수정
    3. 스크롤내리면 해당 구역에 맞는 배너만 출력되게 수정

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,64 @@
 //  header toggle
 function headerToggle() {
-
-    let header = document.getElementById("headerNav");
+  let header = document.getElementById("headerNav");
     if (header.className === "header_Nav") {
+    
       header.className += " responsive";
     } else {
       header.className = "header_Nav";
+      
+
     }
   }
 
+  // header nav state from scroll
+  const home = document.querySelector("#home").offsetTop;
+  const about_me = document.querySelector("#about_me").offsetTop;
+  const skills = document.querySelector("#skills").offsetTop;
+  const project = document.querySelector("#project").offsetTop;
+  const contact = document.querySelector("#contact").offsetTop;
+
+  const homeToggle = document.querySelector(".homeToggle");
+  const aboutToggle = document.querySelector(".aboutToggle");
+  const skillsToggle = document.querySelector(".skillsToggle");
+  const projectToggle = document.querySelector(".projectToggle");
+  const contactToggle = document.querySelector(".contactToggle");
+  function navState(){
+    let location = document.documentElement.scrollTop;
+    if(location < 650){
+      resetToggle();
+      homeToggle.style.display="block";
+
+    }else if(location <= about_me){
+      resetToggle()
+      homeToggle.style.display="";
+      aboutToggle.style.display="block";
+
+     
+    }else if(location <= skills){
+      resetToggle()
+      skillsToggle.style.display="block";
+
+    }else if(location <= project){
+      resetToggle()
+      projectToggle.style.display="block";
+
+    }else if(location <= contact){
+      resetToggle()
+      contactToggle.style.display="block";
+
+    }
+    
+  }
+
+function resetToggle(){
+  homeToggle.style.display="";
+  aboutToggle.style.display="";
+  skillsToggle.style.display="";
+  projectToggle.style.display="";
+  contactToggle.style.display="";
+}
+
+if(outerWidth <= 768){
+  window.addEventListener('scroll',navState)
+}
