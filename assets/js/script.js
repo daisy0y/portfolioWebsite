fc2082d005f6176862559b6c4eca44b2a81fba14
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-09-29 20:11:52 +0900

    200929 add init

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,44 +12,37 @@ function headerToggle() {
   }
 
   // header nav state from scroll
-  const home = document.querySelector("#home").offsetTop;
-  const about_me = document.querySelector("#about_me").offsetTop;
-  const skills = document.querySelector("#skills").offsetTop;
-  const project = document.querySelector("#project").offsetTop;
-  const contact = document.querySelector("#contact").offsetTop;
+const home = document.querySelector("#home").offsetTop;
+const about_me = document.querySelector("#about_me").offsetTop;
+const skills = document.querySelector("#skills").offsetTop;
+const project = document.querySelector("#project").offsetTop;
+const contact = document.querySelector("#contact").offsetTop;
+const homeToggle = document.querySelector(".homeToggle");
+const aboutToggle = document.querySelector(".aboutToggle");
+const skillsToggle = document.querySelector(".skillsToggle");
+const projectToggle = document.querySelector(".projectToggle");
+const contactToggle = document.querySelector(".contactToggle");
 
-  const homeToggle = document.querySelector(".homeToggle");
-  const aboutToggle = document.querySelector(".aboutToggle");
-  const skillsToggle = document.querySelector(".skillsToggle");
-  const projectToggle = document.querySelector(".projectToggle");
-  const contactToggle = document.querySelector(".contactToggle");
-  function navState(){
+function navState(){
     let location = document.documentElement.scrollTop;
     if(location < 650){
       resetToggle();
       homeToggle.style.display="block";
-
     }else if(location <= about_me){
       resetToggle()
       homeToggle.style.display="";
       aboutToggle.style.display="block";
-
-     
     }else if(location <= skills){
       resetToggle()
       skillsToggle.style.display="block";
-
     }else if(location <= project){
       resetToggle()
       projectToggle.style.display="block";
-
     }else if(location <= contact){
       resetToggle()
       contactToggle.style.display="block";
-
     }
-    
-  }
+}
 
 function resetToggle(){
   homeToggle.style.display="";
@@ -58,7 +51,9 @@ function resetToggle(){
   projectToggle.style.display="";
   contactToggle.style.display="";
 }
-
-if(outerWidth <= 768){
-  window.addEventListener('scroll',navState)
+function init(){
+  if(outerWidth <= 768){
+    window.addEventListener('scroll',navState)
+  }
 }
+init();
